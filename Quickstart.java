34c47241f25609404848f5868017ebcf93062af4
JuilletMikael <juillet.mikael@gmail.com>
2025-10-31 09:12:16 +0100

    feat : update quick start with float and double

--- Quickstart.java
+++ Quickstart.java
@@ -1,5 +1,9 @@
 class QuickStart {
-    public static void main (String[] args) {
-        System.out.println("Hello, World.");
+    public static void main(String[] args) {
+        double f = 1.123456789;
+        float d = 1.1234567890123456789f;
+
+        System.out.println("float  : " + f);
+        System.out.println("double : " + d);
     }
 }
